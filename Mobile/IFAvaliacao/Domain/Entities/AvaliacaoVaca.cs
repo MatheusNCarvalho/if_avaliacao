1867be203cb6d5af6083a1f639aa83ad8ec4f896
Matheus <matheusnevesdecarvalho@gmail.com>
2020-05-05 22:33:11 -0300

    Adicionando a pagina de visualização de avalização preenchidas

--- Mobile/IFAvaliacao/Domain/Entities/AvaliacaoVaca.cs
+++ Mobile/IFAvaliacao/Domain/Entities/AvaliacaoVaca.cs
@@ -8,13 +8,16 @@ namespace IFAvaliacao.Domain.Entities
 
         public AvaliacaoVaca(int nameCow, decimal bodyWight)
         {
+            Id = null;
             NameCow = nameCow;
             BodyWight = bodyWight;
         }
         public AvaliacaoVaca(int nameCow, decimal bodyWight, int angulosiodade, int profundidadeCorporal, int forcaLeiteira,
                              int alturaGarupaHipometro, int comprimentoCorpo, int anguloCarupa, int larguraIleo,
                              int larguraIsquio, int anguloCasco, int jarreteLateral, int jarreteTras, int ubereFirmeza,
-                             int uberePosterior, int alturaUbere, int ligamentoCentral, int posicaoTetos, DateTime dataHoraIinicio)
+                             int uberePosterior, int alturaUbere, int ligamentoCentral, int posicaoTetos, DateTime dataHoraIinicio,
+                             decimal comprimentoTeto1, decimal comprimentoTeto2, decimal comprimentoTeto3, decimal comprimentoTeto4,
+                             decimal diametroTeto1, decimal diametroTeto2, decimal diametroTeto3, decimal diametroTeto4)
         {
             NameCow = nameCow;
             BodyWight = bodyWight;
@@ -35,6 +38,14 @@ namespace IFAvaliacao.Domain.Entities
             LigamentoCentral = ligamentoCentral;
             PosicaoTetos = posicaoTetos;
             DataHoraInicio = dataHoraIinicio;
+            ComprimentoTeto1 = comprimentoTeto1;
+            ComprimentoTeto2 = comprimentoTeto2;
+            ComprimentoTeto3 = comprimentoTeto3;
+            ComprimentoTeto4 = comprimentoTeto4;
+            DiametroTeto1 = diametroTeto1;
+            DiametroTeto2 = diametroTeto2;
+            DiametroTeto3 = diametroTeto3;
+            DiametroTeto4 = diametroTeto4;
         }
 
         public DateTime DataHoraInicio { get; set; }
@@ -72,5 +83,20 @@ namespace IFAvaliacao.Domain.Entities
         public int LigamentoCentral { get; set; }
 
         public int PosicaoTetos { get; set; }
+        public decimal ComprimentoTeto1 { get; set; }
+
+        public decimal ComprimentoTeto2 { get; set; }
+
+        public decimal ComprimentoTeto3 { get; set; }
+
+        public decimal ComprimentoTeto4 { get; set; }
+
+        public decimal DiametroTeto1 { get; set; }
+
+        public decimal DiametroTeto2 { get; set; }
+
+        public decimal DiametroTeto3 { get; set; }
+
+        public decimal DiametroTeto4 { get; set; }
     }
 }
--- Mobile/IFAvaliacao/ViewModels/PreenchimentoViewModel.cs
+++ Mobile/IFAvaliacao/ViewModels/PreenchimentoViewModel.cs
@@ -10,137 +10,123 @@ namespace IFAvaliacao.ViewModels
 {
     public class PreenchimentoViewModel : ViewModelBase
     {
-        private readonly ICommand _tapCommand;
         private readonly IAvaliacaoRepository _avaliacaoRepository;
         public PreenchimentoViewModel(INavigationService navigationService, IAvaliacaoRepository avaliacaoRepository) : base(navigationService)
         {
             _avaliacaoRepository = avaliacaoRepository;
-            _tapCommand = new Command(OnTapped);
+            TapCommand = new Command(OnTapped);
             SaveCommand = new Command(ExecuteSaveCommand);
             SliderMinimum = 1.0;
             DataHoraInicio = DateTime.Now;
         }
 
-        public ICommand TapCommand { get => _tapCommand; }
+        public ICommand TapCommand { get; }
         public ICommand SaveCommand { get; }
 
         public DateTime DataHoraInicio { get; set; }
 
-        private double _angulosidade = 0.0;
-        public double Angulosiodade
-        {
-            get => _angulosidade;
-            set => SetProperty(ref _angulosidade, value);
-        }
+        private bool _podeSalvar = true;
+        public bool PodeSalvar { get => _podeSalvar; set => SetProperty(ref _podeSalvar, value); }
 
-        private double _profundidadeCorporal = 0.0;
-        public double ProfundidadeCorporal
-        {
-            get => _profundidadeCorporal;
-            set => SetProperty(ref _profundidadeCorporal, value);
-        }
+        private double _angulosidade;
+        public double Angulosiodade { get => _angulosidade; set => SetProperty(ref _angulosidade, value); }
 
-        private double _forcaLeiteira = 0.0;
-        public double ForcaLeiteira
-        {
-            get => _forcaLeiteira;
-            set => SetProperty(ref _forcaLeiteira, value);
-        }
+        private double _profundidadeCorporal;
+        public double ProfundidadeCorporal { get => _profundidadeCorporal; set => SetProperty(ref _profundidadeCorporal, value); }
 
-        private double _alturaGarupaHipometro = 0.0;
-        public double AlturaGarupaHipometro
-        {
-            get => _alturaGarupaHipometro;
-            set => SetProperty(ref _alturaGarupaHipometro, value);
-        }
-        private double _comprimentoCorpo = 0.0;
+        private double _forcaLeiteira;
+        public double ForcaLeiteira { get => _forcaLeiteira; set => SetProperty(ref _forcaLeiteira, value); }
+
+        private double _alturaGarupaHipometro;
+        public double AlturaGarupaHipometro { get => _alturaGarupaHipometro; set => SetProperty(ref _alturaGarupaHipometro, value); }
+        private double _comprimentoCorpo;
         public double ComprimentoCorpo { get => _comprimentoCorpo; set => SetProperty(ref _comprimentoCorpo, value); }
 
-        private double _anguloCarupa = 0.0;
+        private double _anguloCarupa;
         public double AnguloCarupa { get => _anguloCarupa; set => SetProperty(ref _anguloCarupa, value); }
 
-        private double _larguraIleo = 0.0;
+        private double _larguraIleo;
         public double LarguraIleo { get => _larguraIleo; set => SetProperty(ref _larguraIleo, value); }
 
-        private double _larguraIsquio = 0.0;
+        private double _larguraIsquio;
         public double LarguraIsquio { get => _larguraIsquio; set => SetProperty(ref _larguraIsquio, value); }
 
-        private double _anguloCasco = 0.0;
+        private double _anguloCasco;
         public double AnguloCasco { get => _anguloCasco; set => SetProperty(ref _anguloCasco, value); }
 
-        private double _jarreteLateral = 0.0;
+        private double _jarreteLateral;
         public double JarreteLateral { get => _jarreteLateral; set => SetProperty(ref _jarreteLateral, value); }
 
-        private double _jarreteTras = 0.0;
+        private double _jarreteTras;
         public double JarreteTras { get => _jarreteTras; set => SetProperty(ref _jarreteTras, value); }
 
-        private double _ubereFirmeza = 0.0;
+        private double _ubereFirmeza;
         public double UbereFirmeza { get => _ubereFirmeza; set => SetProperty(ref _ubereFirmeza, value); }
 
-        private double _uberePosterior = 0.0;
+        private double _uberePosterior;
         public double UberePosterior { get => _uberePosterior; set => SetProperty(ref _uberePosterior, value); }
 
-        private double _alturaUltere = 0.0;
+        private double _alturaUltere;
         public double AlturaUbere { get => _alturaUltere; set => SetProperty(ref _alturaUltere, value); }
 
-        private double _ligamentoCentral = 0.0;
+        private double _ligamentoCentral;
         public double LigamentoCentral { get => _ligamentoCentral; set => SetProperty(ref _ligamentoCentral, value); }
 
-        private double _posicaoTetos = 0.0;
+        private double _posicaoTetos;
         public double PosicaoTetos { get => _posicaoTetos; set => SetProperty(ref _posicaoTetos, value); }
 
-        private decimal _comprimentoTeto1 = 0;
+        private decimal _comprimentoTeto1;
         public decimal ComprimentoTeto1 { get => _comprimentoTeto1; set => SetProperty(ref _comprimentoTeto1, value); }
 
-        private decimal _comprimentoTeto2 = 0;
+        private decimal _comprimentoTeto2;
         public decimal ComprimentoTeto2 { get => _comprimentoTeto2; set => SetProperty(ref _comprimentoTeto2, value); }
 
-        private decimal _comprimentoTeto3 = 0;
+        private decimal _comprimentoTeto3;
         public decimal ComprimentoTeto3 { get => _comprimentoTeto3; set => SetProperty(ref _comprimentoTeto3, value); }
 
-        private decimal _comprimentoTeto4 = 0;
+        private decimal _comprimentoTeto4;
         public decimal ComprimentoTeto4 { get => _comprimentoTeto4; set => SetProperty(ref _comprimentoTeto4, value); }
 
-        private decimal _diametroTeto1 = 0;
+        private decimal _diametroTeto1;
         public decimal DiametroTeto1 { get => _diametroTeto1; set => SetProperty(ref _diametroTeto1, value); }
 
-        private decimal _diametroTeto2 = 0;
+        private decimal _diametroTeto2;
         public decimal DiametroTeto2 { get => _diametroTeto2; set => SetProperty(ref _diametroTeto2, value); }
 
-        private decimal _diametroTeto3 = 0;
+        private decimal _diametroTeto3;
         public decimal DiametroTeto3 { get => _diametroTeto3; set => SetProperty(ref _diametroTeto3, value); }
 
-        private decimal _diametroTeto4 = 0;
+        private decimal _diametroTeto4;
         public decimal DiametroTeto4 { get => _diametroTeto4; set => SetProperty(ref _diametroTeto4, value); }
 
         private int _nameCow;
-        public int NameCow
-        {
-            get => _nameCow;
-            set => SetProperty(ref _nameCow, value);
-        }
+        public int NameCow { get => _nameCow; set => SetProperty(ref _nameCow, value); }
 
-        private decimal _bodyWight = 0;
-        public decimal BodyWight
-        {
-            get => _bodyWight;
-            set => SetProperty(ref _bodyWight, value);
-        }
+        private decimal _bodyWight;
+        public decimal BodyWight { get => _bodyWight; set => SetProperty(ref _bodyWight, value); }
 
         public double SliderMinimum { get; set; }
         public double SliderMaximum { get; set; }
 
 
-        public override void OnNavigatedTo(INavigationParameters parameters)
+        public override async void OnNavigatedTo(INavigationParameters parameters)
         {
             try
             {
                 var profileCow = (AvaliacaoVaca)parameters[nameof(AvaliacaoVaca)];
 
-                if (profileCow != null)
+                if (profileCow == null)
                 {
-                    BodyWight = profileCow.BodyWight;
-                    NameCow = profileCow.NameCow;
+                    await NavigationService.GoBackAsync();
+                    return;
+                }
+
+                BodyWight = profileCow.BodyWight;
+                NameCow = profileCow.NameCow;
+
+                if (profileCow.Id.HasValue())
+                {
+                    Loading(profileCow);
                 }
             }
             catch (Exception ex)
@@ -158,14 +144,16 @@ namespace IFAvaliacao.ViewModels
                                                   LarguraIleo.DoubleToInt(), LarguraIsquio.DoubleToInt(), AnguloCasco.DoubleToInt(),
                                                   JarreteLateral.DoubleToInt(), JarreteTras.DoubleToInt(), UbereFirmeza.DoubleToInt(),
                                                   UberePosterior.DoubleToInt(), AlturaUbere.DoubleToInt(), LigamentoCentral.DoubleToInt(),
-                                                  PosicaoTetos.DoubleToInt(), DataHoraInicio)
-                                {
-                                    DataHoraFim = DateTime.Now
-                                };
+                                                  PosicaoTetos.DoubleToInt(), DataHoraInicio, ComprimentoTeto1, ComprimentoTeto2,
+                                                  ComprimentoTeto3, ComprimentoTeto4, DiametroTeto1, DiametroTeto2,
+                                                  DiametroTeto3, DiametroTeto4)
+                {
+                    DataHoraFim = DateTime.Now
+                };
 
                 if (await _avaliacaoRepository.AddAsync(avaliacao))
                 {
-                    ToastSuccess("Valiação adicionada com sucesso!");
+                    ToastSuccess("Avaliação adicionada com sucesso!");
                     await NavigationService.GoBackToRootAsync();
                     return;
                 }
@@ -222,6 +210,35 @@ namespace IFAvaliacao.ViewModels
                     PosicaoTetos = double.Parse(value[1]);
                     break;
             }
+        }
+
+        private void Loading(AvaliacaoVaca profileCow)
+        {
+            Angulosiodade = profileCow.Angulosiodade;
+            ProfundidadeCorporal = profileCow.ProfundidadeCorporal;
+            ForcaLeiteira = profileCow.ForcaLeiteira;
+            AlturaGarupaHipometro = profileCow.AlturaGarupaHipometro;
+            ComprimentoCorpo = profileCow.ComprimentoCorpo;
+            AnguloCarupa = profileCow.AnguloCarupa;
+            LarguraIleo = profileCow.LarguraIleo;
+            LarguraIsquio = profileCow.LarguraIsquio;
+            AnguloCasco = profileCow.AnguloCasco;
+            JarreteLateral = profileCow.JarreteLateral;
+            JarreteTras = profileCow.JarreteTras;
+            UbereFirmeza = profileCow.UbereFirmeza;
+            UberePosterior = profileCow.UberePosterior;
+            AlturaUbere = profileCow.AlturaUbere;
+            LigamentoCentral = profileCow.LigamentoCentral;
+            PosicaoTetos = profileCow.PosicaoTetos;
+            ComprimentoTeto1 = profileCow.ComprimentoTeto1;
+            ComprimentoTeto2 = profileCow.ComprimentoTeto2;
+            ComprimentoTeto3 = profileCow.ComprimentoTeto3;
+            ComprimentoTeto4 = profileCow.ComprimentoTeto4;
+            DiametroTeto1 = profileCow.DiametroTeto1;
+            DiametroTeto2 = profileCow.DiametroTeto2;
+            DiametroTeto3 = profileCow.DiametroTeto3;
+            DiametroTeto4 = profileCow.DiametroTeto4;
+            PodeSalvar = false;
         }
     }
 }
